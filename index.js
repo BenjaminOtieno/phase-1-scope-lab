222b09f9f593495cf28d08d4e529e6a1a4f47c0c
BenjaminOtieno <benjamin.otieno@moringaschool.com>
2022-08-18 00:52:58 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+var bestCustomer;
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+bestCustomer;
+
+const leastFavoriteCustomer = 'tom';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'harry';
+}
